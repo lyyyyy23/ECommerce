0ab920ad2ce10db92621576a26bc337ba40a5aee
lyyyyy23 <ellyssagolfo@gmail.com>
2023-09-30 14:22:11 +0800

    Lab3

--- app/Controllers/ShoeCollectionController.php
+++ app/Controllers/ShoeCollectionController.php
@@ -15,4 +15,28 @@ class ShoeCollectionController extends BaseController
     {
         return view('shoes/about');
     }
+    public function blog()
+    {
+        return view('shoes/blog');
+    }
+    public function contact()
+    {
+        return view('shoes/contact');
+    }
+    public function cart()
+    {
+        return view('shoes/cart');
+    }
+    public function shop()
+    {
+        return view('shoes/shop');
+    }
+    public function singleProd()
+    {
+        return view('shoes/single-product');
+    }
+    public function checkout()
+    {
+        return view('shoes/checkout');
+    }
 }
